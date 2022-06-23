369243d7779fc2120e1d6bc76a12ded2deb1f74c
Ayjmax <ayj.max@gmail.com>
2022-06-22 21:49:44 -0600

    Changed var names to match lint

--- Filter_Scripts/500Cities_Filter.R
+++ Filter_Scripts/500Cities_Filter.R
@@ -1,10 +1,10 @@
 #Reading in csv file(s)
-FIVE_CASTHMA <- read.csv("./Raw_Data/500Cities_Files/500_Cities__Current_asthma_among_adults_aged___18_years(1).csv")
+five_casthma <- read.csv("./Raw_Data/500Cities_Files/500_Cities__Current_asthma_among_adults_aged___18_years(1).csv")
 
 #Removing columns
-FIVE_CASTHMA = subset(FIVE_CASTHMA, select = -c(StateDesc, StateAbbr, Category, Measure, DataValueTypeID,
+five_casthma <- subset(five_casthma, select = -c(StateDesc, StateAbbr, Category, Measure, DataValueTypeID,
                                                 Data_Value_Footnote_Symbol, Data_Value_Footnote, DataSource,
                                                 CategoryID, MeasureId, Short_Question_Text))
 
 #Writing to file
-write.csv(FIVE_CASTHMA, file="./PASS_Data/CDC/500Cities/PASS_500Cities_Current_Asthma.csv")
+write.csv(five_casthma, file="./PASS_Data/CDC/500Cities/PASS_500Cities_Current_Asthma.csv")
