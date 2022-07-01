fd6dcb26dcea25c7924686011f3cc5839e010976
Ayjmax <ayj.max@gmail.com>
2022-06-30 23:23:33 -0600

    Implemented new verify_work_dir method

--- Webscrapers/CDC_Webscrape_V1.0.py
+++ Webscrapers/CDC_Webscrape_V1.0.py
@@ -5,18 +5,16 @@ from pathlib import Path
 from os import chdir, getcwd
 from sodapy import Socrata
 
+from verify_work_dir import verify_work_dir
+
 #Socrata API hosts various government data
 Socrata_url = "https://api.us.socrata.com/api/catalog/v1?names={}&domains=chronicdata.cdc.gov"
 formats = {"Census Tract": "Ce", "County": "Co"} #"ZCTA": "ZCTA"
 categories = ["CASTHMA", "RISKBEH", "PREVENT"]
 years = [2021, 2020] #add more when necessary
 
-#Change work directory as necessary
-work_dir = "C:/Mountaintop" #Change if needed
-if (getcwd() != work_dir):
-    chdir(work_dir)
 
-def getData(ID, form, year):
+def get_api_data(ID, form, year):
     #Check if file dir exists; if not, create new dir
     base_dir = f"{work_dir}/Raw_Data/API_PLACES_Files/{year}/{form}"
     Path(base_dir).mkdir(parents=True, exist_ok=True)
@@ -36,17 +34,25 @@ def getData(ID, form, year):
     df = pd.DataFrame.from_records(results_prevent)
     df.to_csv(f"{base_dir}/Raw_PLACES_{abbr}_PREVENT_{year}.csv", index=False)
 
-for form in formats:
-    for year in years:
-        title = f"PLACES: Local Data for Better Health, {form} Data {year} release"
-        response = requests.get(Socrata_url.format(title))
-        print(form, year)
-        if (response.status_code == 200):
-            print('API responded')
-            data = response.text
-            parsed_json = json.loads(data)
-            ID = parsed_json['results'][0]['resource']['id']
-            getData(ID, form, str(year))
-        else:
-            print('ERROR')
-            exit(1)
+def get_places_data():
+    verify_work_dir(__file__)
+    
+    for form in formats:
+        for year in years:
+            title = f"PLACES: Local Data for Better Health, {form} Data {year} release"
+            response = requests.get(Socrata_url.format(title))
+            print(form, year)
+            if (response.status_code == 200):
+                print('API responded')
+                data = response.text
+                parsed_json = json.loads(data)
+                ID = parsed_json['results'][0]['resource']['id']
+                get_api_data(ID, form, str(year))
+            else:
+                print('ERROR')
+                exit(1)
+
+
+
+if __name__ == "__main__":
+    get_places_data()
